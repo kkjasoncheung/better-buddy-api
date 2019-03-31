029183e602789d64c2fd62615e72937ec898e351
kkjasoncheung <kkjason.cheung@mail.utoronto.ca>
2019-03-31 19:15:34 -0400

    Adds CreateUser method with password hashing using bcrypt

--- models/user.go
+++ models/user.go
@@ -1,7 +1,11 @@
 package models
 
 import (
+	"log"
+
 	"github.com/jinzhu/gorm"
+	"github.com/kkjasoncheung/better-buddy-api/db"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
@@ -14,4 +18,38 @@ type User struct {
 	BirthDay        string `json:"birthday"`
 	Gender          string `json:"gender"`
 	DisplayPhotoURL string `json:"display_photo_url"`
+}
+
+// GetAllUsers returns all users.
+func (u User) GetAllUsers() []User {
+	users := []User{}
+	// Query the DB
+	// db := db.GetDb()
+	return users
+}
+
+// CreateUser creates a new user and stores it in the database.
+func (u User) CreateUser(firstName, lastName, username, password, email, birthday, gender, displayPhotoURL string) {
+	db := db.GetDb()
+
+	user := User{
+		FirstName:       firstName,
+		LastName:        lastName,
+		Username:        username,
+		PasswordDigest:  HashPassword(password),
+		Email:           email,
+		BirthDay:        birthday,
+		Gender:          gender,
+		DisplayPhotoURL: displayPhotoURL,
+	}
+	db.Create(&user)
+}
+
+// HashPassword hashes the password passed in with bcrypt.
+func HashPassword(password string) string {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(hashedPassword)
 }
