f63aaab2a2104b1906bd35b0e7ef024ad6efb16a
kkjasoncheung <kkjason.cheung@mail.utoronto.ca>
2019-03-31 16:27:32 -0400

    Creates a User struct

---
+++ models/user.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+type User struct {
+	gorm.Model
+	FirstName       string `json:"first_name"`
+	LastName        string `json:"last_name"`
+	Username        string `json:"username"`
+	PasswordDigest  string
+	Email           string `json:"email"`
+	BirthDay        string `json:"birthday"`
+	Gender          string `json:"gender"`
+	DisplayPhotoURL string `json:"display_photo_url"`
+}
