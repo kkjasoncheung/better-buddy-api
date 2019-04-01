017ae9c6e2c69302635d3f918071d4011fe1fc72
kkjasoncheung <kkjason.cheung@mail.utoronto.ca>
2019-03-31 23:20:48 -0400

    Adds indexing

--- migrations/migration.go
+++ migrations/migration.go
@@ -8,4 +8,9 @@ import (
 // MigrateSchema migrates any schemas at initialization.
 func MigrateSchema(db *gorm.DB) {
 	db.AutoMigrate(models.User{})
+	db.Model(models.User{}).AddIndex("idx_first_name", "first_name")
+	db.Model(models.User{}).AddIndex("idx_last_name", "last_name")
+
+	db.AutoMigrate(models.Companion{})
+	db.Model(models.User{}).AddIndex("idx_name", "name")
 }
