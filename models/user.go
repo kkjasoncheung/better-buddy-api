25e5ccb47def6e9d9c76082fe82271602f3fe3bc
kkjasoncheung <kkjason.cheung@mail.utoronto.ca>
2019-03-31 22:13:31 -0400

    Finishes adding CRUD actions to User model

--- models/user.go
+++ models/user.go
@@ -20,14 +20,6 @@ type User struct {
 	DisplayPhotoURL string `json:"display_photo_url"`
 }
 
-// GetAllUsers returns all users.
-func (u User) GetAllUsers() []User {
-	users := []User{}
-	// Query the DB
-	// db := db.GetDb()
-	return users
-}
-
 // CreateUser creates a new user and stores it in the database.
 func (u User) CreateUser(firstName, lastName, username, password, email, birthday, gender, displayPhotoURL string) {
 	db := db.GetDb()
@@ -43,6 +35,61 @@ func (u User) CreateUser(firstName, lastName, username, password, email, birthda
 		DisplayPhotoURL: displayPhotoURL,
 	}
 	db.Create(&user)
+}
+
+// GetAllUsers returns all users.
+func (u User) GetAllUsers() []User {
+	users := []User{}
+	// Query the DB
+	// db := db.GetDb()
+	return users
+}
+
+// FindByID finds a user by id.
+func (u User) FindByID(id int) *User {
+	db := db.GetDb()
+	user := new(User)
+	db.First(&user, id)
+	return user
+}
+
+// UpdateByID updates a user's attributes by id.
+func (u User) UpdateByID(id int, fields map[string]string) *User {
+	db := db.GetDb()
+
+	user := new(User)
+	user = user.FindByID(id)
+
+	for key, value := range fields {
+		switch key {
+		case "FirstName":
+			user.FirstName = value
+		case "LastName":
+			user.LastName = value
+		case "Username":
+			user.Username = value
+		case "Email":
+			user.Email = value
+		case "BirthDay":
+			user.BirthDay = value
+		case "Gender":
+			user.Gender = value
+		case "DisplayPhotoUrl":
+			user.DisplayPhotoURL = value
+		}
+	}
+	db.Save(&user)
+
+	return user
+}
+
+// DeleteByID deletes a user by id.
+func (u User) DeleteByID(id int) *User {
+	db := db.GetDb()
+	user := new(User)
+	user = user.FindByID(id)
+	db.Delete(&user)
+	return user
 }
 
 // HashPassword hashes the password passed in with bcrypt.
