5fa52f40e48d30adc2341ccb3a9d1e0142ebff73
kkjasoncheung <kkjason.cheung@mail.utoronto.ca>
2019-04-07 01:16:25 -0400

    Quick fix for improper function call in main.go

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/kkjasoncheung/better-buddy-api/db"
 	"github.com/kkjasoncheung/better-buddy-api/migrations"
-	"github.com/kkjasoncheung/better-buddy-api/models"
 )
 
 func main() {
@@ -12,14 +11,6 @@ func main() {
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
-		})
-	})
-	r.GET("/user", func(c *gin.Context) {
-		user := new(models.User)
-		user.CreateUser()
-		c.JSON(200, gin.H{
-			"status":  200,
-			"message": "Created user.",
 		})
 	})
 	db := db.Init()
