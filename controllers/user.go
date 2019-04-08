70f248ad24fff81e9b96e955bc0d18f49dd8d668
kkjasoncheung <kkjason.cheung@mail.utoronto.ca>
2019-04-07 23:36:44 -0400

    Adds router handler for GET /user and /user/:id

--- controllers/user.go
+++ controllers/user.go
@@ -1,6 +1,9 @@
 package controllers
 
 import (
+	"net/http"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 	"github.com/kkjasoncheung/better-buddy-api/models"
 )
@@ -10,16 +13,30 @@ type UserController struct{}
 
 var userModel = new(models.User)
 
-// TODO: Implement Index, Show, Edit, Destroy actions.
+// Retrieve handles GET /user. Returns all users.
+func (u UserController) Retrieve(c *gin.Context) {
+	// Retrieve all records for user.
+	users := userModel.GetAllUsers()
+	c.JSON(http.StatusOK, gin.H{"users": users})
+}
 
 // RetrieveByID handles GET /user/:id. Finds a user by ID.
 func (u UserController) RetrieveByID(c *gin.Context) {
 	if c.Param("id") != "" {
 		// Find the user and return
+		if newVal, err := strconv.ParseUint(c.Param("id"), 10, 32); err == nil {
+			user := userModel.FindByID(uint(newVal))
+			c.JSON(http.StatusOK, gin.H{"user": user})
+			return
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "Error to retrieve user", "error": err})
+			c.Abort()
+			return
+		}
 	}
+	c.JSON(http.StatusBadRequest, gin.H{"message": "bad request"})
+	c.Abort()
+	return
 }
 
-// Retrieve handles GET /user. Returns all users.
-func (u UserController) Retrieve(c *gin.Context) {
-	// Retrieve all records for user.
-}
+// TODO: Implement PATCH user/:id, POST user, DELETE user/:id
